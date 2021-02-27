d7122634cc62b72f8d6038a2682ae9b88ea423ed
Radoslav Hubenov <rrhubenov@gmail.com>
2021-02-27 13:18:05 +0200

    add labels for nodes and rels

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from djongo import models
 
+from neomodel import *
+
 class Blog(models.Model):
     name = models.CharField(max_length=100)
 
@@ -12,3 +14,16 @@ class Entry(models.Model):
     )    
     headline = models.CharField(max_length=255)    
 
+class ClusterOwnershipRel(StructuredRel):
+    pass
+
+class Device(StructuredNode):
+    last_hour_uptime = FloatProperty(default=100)
+
+    parent_cluster = Relationship('Cluster', 'PARENTS', model=ClusterOwnershipRel)
+
+
+class Cluster(StructuredNode):
+    average_uptime = FloatProperty(default=100)
+
+    parent_cluster = Relationship('Cluster', 'PARENTS', model=ClusterOwnershipRel)
