5bf0eea1e8f361442fbf49cc0aad0ed0608e7485
Radoslav Hubenov <rrhubenov@gmail.com>
2021-02-27 11:38:18 +0200

    basic get post for pings

--- interface/views.py
+++ interface/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from rest_framework import views, serializers
+from rest_framework.response import Response
+
 # Create your views here.
+
+
+class CreatePingView(views.APIView):
+
+    class InputSerializer(serializers.Serializer):
+        location = serializers.CharField()
+
+    def post(self, request):
+        input_serializer = self.InputSerializer(data=request.data)
+
+        return Response()
+
+
+class GetPingsView(views.APIView):
+
+    def get(self, request):
+
+        return Response()
