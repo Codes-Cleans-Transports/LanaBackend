a221e8b11614feccc884ef2f2e1721e2de246e2e
Radoslav Hubenov <rrhubenov@gmail.com>
2021-02-27 15:04:21 +0200

    remove django-neomodel

--- LanaBackend/settings.py
+++ LanaBackend/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_neomodel',
     'rest_framework',
     'core',
     'interface',
