caecbd779d0a82c5c0f67d0b829e08d0910e5852
Radoslav Hubenov <rrhubenov@gmail.com>
2021-02-27 18:49:18 +0200

    call business logic on post

--- interface/views.py
+++ interface/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from datetime import datetime
 
 from rest_framework import views, serializers
 from rest_framework.response import Response
+
+from core.logic import savePing, DevicePing
 
 # Create your views here.
 
@@ -11,8 +14,11 @@ class CreatePingView(views.APIView):
     class InputSerializer(serializers.Serializer):
         location = serializers.CharField()
 
-    def post(self, request):
+    def post(self, request, cluster_id, device_id):
         input_serializer = self.InputSerializer(data=request.data)
+        input_serializer.is_valid()
+
+        savePing(DevicePing(id=device_id, clusterId=cluster_id, location=input_serializer['location'], date=datetime.now()))
 
         return Response()
 
