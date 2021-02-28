45a4fb5add6866735de215745f4e16e63681736b
Viktor Varbanov <viktor.varbanov@trading212.com>
2021-02-28 10:34:01 +0200

    Fixed generated data

--- datageneration/data_generation.py
+++ datageneration/data_generation.py
@@ -16,7 +16,7 @@ def generate_sofia_data():
     sofia_bucket_data = [[0 for x in range(40)] for y in range(sofia_points)]
     for i in range(sofia_points):
         for j in range(40):
-            sofia_bucket_data[i].append(random() * 100)
+            sofia_bucket_data[i].append(random() * 20 + 80)
     sofia_data = []
     for i in range(sofia_points):
         sofia_data.append(DeviceData(id=1,
@@ -61,38 +61,32 @@ def generate_burgas_data():
 
 def generate_sofia_location_data():
     sofia_data = []
-    current_x = 0
-    current_y = 0
     serdika = (42.697684, 23.321857)
     for i in range(sofia_points):
-        current_x += 7 / 100 / 486
-        current_y += 9 / 100 / 486
-        sofia_data.append((serdika[0] + current_x, serdika[1] + current_y))
-        sofia_data.append((serdika[0] - current_x, serdika[1] - current_y))
+        sofia_data.append((serdika[0] + random() * 7 / 100, serdika[1] + random() * 9 / 100))
+        sofia_data.append((serdika[0] - random() * 7 / 100, serdika[1] - random() * 9 / 100))
+        sofia_data.append((serdika[0] + random() * 7 / 100, serdika[1] - random() * 9 / 100))
+        sofia_data.append((serdika[0] - random() * 7 / 100, serdika[1] + random() * 9 / 100))
     return sofia_data
 
 
 def generate_varna_location_data():
     varna_data = []
-    current_x = 0
-    current_y = 0
     grand_mall = (43.217821, 27.902738)
     for i in range(varna_points):
-        current_x += 1 / 100 / 236
-        current_y += 1 / 100 / 236
-        varna_data.append((grand_mall[0] + current_x, grand_mall[1] + current_y))
-        varna_data.append((grand_mall[0] - current_x, grand_mall[1] - current_y))
+        varna_data.append((grand_mall[0] + random() / 100, grand_mall[1] + random() / 100))
+        varna_data.append((grand_mall[0] - random() / 100, grand_mall[1] - random() / 100))
+        varna_data.append((grand_mall[0] - random() / 100, grand_mall[1] + random() / 100))
+        varna_data.append((grand_mall[0] + random() / 100, grand_mall[1] - random() / 100))
     return varna_data
 
 
 def generate_burgas_location_data():
     burgas_data = []
-    current_x = 0
-    current_y = 0
     aqua = (42.511620, 27.465627)
     for i in range(burgas_points):
-        current_x += 2 / 100 / 208
-        current_y += 1 / 100 / 208
-        burgas_data.append((aqua[0] + current_x, aqua[1] + current_y))
-        burgas_data.append((aqua[0] - current_x, aqua[1] - current_y))
+        burgas_data.append((aqua[0] + random() / 50, aqua[1] + random() / 100))
+        burgas_data.append((aqua[0] - random() / 50, aqua[1] - random() / 100))
+        burgas_data.append((aqua[0] + random() / 50, aqua[1] - random() / 100))
+        burgas_data.append((aqua[0] - random() / 50, aqua[1] + random() / 100))
     return burgas_data
