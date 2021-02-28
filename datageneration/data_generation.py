28a9c515df8edf8f57ca3c2b69db6d2860b6e91d
Radoslav Hubenov <rrhubenov@gmail.com>
2021-02-28 11:47:06 +0200

    locations fix

--- datageneration/data_generation.py
+++ datageneration/data_generation.py
@@ -89,7 +89,7 @@ def generate_burgas_data():
 def generate_sofia_location_data():
     sofia_data = []
     serdika = (42.697684, 23.321857)
-    for i in range(sofia_points):
+    for i in range(int(sofia_points/3)):
         sofia_data.append((serdika[0] + random() * 7 / 100, serdika[1] + random() * 9 / 100))
         sofia_data.append((serdika[0] - random() * 7 / 100, serdika[1] - random() * 9 / 100))
         sofia_data.append((serdika[0] + random() * 7 / 100, serdika[1] - random() * 9 / 100))
@@ -100,7 +100,7 @@ def generate_sofia_location_data():
 def generate_varna_location_data():
     varna_data = []
     grand_mall = (43.217821, 27.902738)
-    for i in range(varna_points):
+    for i in range(int(varna_points/3)):
         varna_data.append((grand_mall[0] + random() / 100, grand_mall[1] + random() / 100))
         varna_data.append((grand_mall[0] - random() / 100, grand_mall[1] - random() / 100))
         varna_data.append((grand_mall[0] - random() / 100, grand_mall[1] + random() / 100))
@@ -111,7 +111,7 @@ def generate_varna_location_data():
 def generate_burgas_location_data():
     burgas_data = []
     aqua = (42.511620, 27.465627)
-    for i in range(burgas_points):
+    for i in range(int(burgas_points/3)):
         burgas_data.append((aqua[0] + random() / 50, aqua[1] + random() / 100))
         burgas_data.append((aqua[0] - random() / 50, aqua[1] - random() / 100))
         burgas_data.append((aqua[0] + random() / 50, aqua[1] - random() / 100))
