1e2af6f0ffc3ae46987dc14f88b87f64909675ec
Viktor Varbanov <viktor.varbanov@trading212.com>
2021-02-28 10:24:49 +0200

    Kmeans accepts and returns nodes

--- grouping/Kmeans.py
+++ grouping/Kmeans.py
@@ -1,29 +1,27 @@
 import math
 
 import numpy
-# from sklearn.cluster import KMeans
-
-from grouping.models import Cluster
+from sklearn.cluster import KMeans
 
 from core.logic import Node
 
 
-def get_clusters(devices: [], k):
+def get_clusters(nodes: [], k):
     kmean = KMeans(n_clusters=k)
     locations = []
-    for i in devices:
+    for i in nodes:
         locations.append(i.location)
     kmean.fit(locations)
     clusters_sum = numpy.ones(k)
     clusters_number = numpy.ones(k)
     cluster_uptime = numpy.ones(k)
     cluster_radius = numpy.ones(k)
+    cluster_children = numpy.ones(k)
 
-    children = numpy.ones(k)
-    
-    for i in range(len(devices)):
-        clusters_sum[kmean.labels_[i]] += devices[i].average_uptime
+    for i in range(len(nodes)):
+        clusters_sum[kmean.labels_[i]] += nodes[i].average_uptime
         clusters_number[kmean.labels_[i]] += 1
+        cluster_children[kmean.labels_[i]] += nodes[i].children
         x1 = locations[i][0]
         y1 = locations[i][1]
         x2 = kmean.cluster_centers_[kmean.labels_[i]][0]
@@ -35,13 +33,13 @@ def get_clusters(devices: [], k):
     for i in range(k):
         cluster_uptime[i] = clusters_sum[i] / clusters_number[i]
 
-    nodes = []
+    final_nodes = []
 
     for i in range(k):
-        nodes.append(Node(location=kmean.cluster_centers_[i],
-                          average_uptime=cluster_uptime[i],
-                          radius=cluster_radius[i],
-                          # TODO: Fill children with nodes that belong to this new group
-                          children=[]))
+        final_nodes.append(Node(location=kmean.cluster_centers_[i],
+                                average_uptime=cluster_uptime[i],
+                                radius=cluster_radius[i],
+                                children=cluster_children[i],
+                                ))
 
-    return clusters
+    return final_nodes
