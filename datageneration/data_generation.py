077d67522ac489e28f7908cf5b0c6e6526f9bd8c
Radoslav Hubenov <rrhubenov@gmail.com>
2021-02-28 11:33:20 +0200

    generate devices in cities

--- datageneration/data_generation.py
+++ datageneration/data_generation.py
@@ -1,6 +1,9 @@
 from random import random
 
 from core.models import DeviceData
+from core.logic import addPingCustom, createDevice, DevicePing
+
+from datetime import datetime, timedelta
 
 sofia_points = 486
 varna_points = 236
@@ -8,7 +11,28 @@ burgas_points = 208
 
 
 def generate_data():
-    return generate_sofia_data() + generate_varna_data() + generate_burgas_data()
+    data = generate_sofia_data() + generate_varna_data() + generate_burgas_data()
+    for device in data:
+        current_time = datetime.now()
+        ping_uptimes = device.buckets
+        device.buckets = [ \
+            # 60 seconds, overflow every hour hold up to 6 hours
+            {"currentSequence": 0, "overflow": 60, "maxSize": 60*6, "data": []}, \
+            # Overflow every 24 pings/hours hold up to 7 days
+            {"currentSequence": 0, "overflow": 24, "maxSize": 7*24, "data": []}, \
+            # Overflow every 7 pings/1 week hold up to 5 weeks
+            {"currentSequence": 0, "overflow": 7, "maxSize": 7*5, "data": []}, \
+            # Overflow every 4 weeks hold up to 3 months
+            {"currentSequence": 0, "overflow": 4, "maxSize": 4*3, "data": []}, \
+            # Overflow every 3 months hold up to a year
+            {"currentSequence": 0, "overflow": 3, "maxSize": 3*4, "data": []}, \
+        ]
+
+        for ping in ping_uptimes:
+            addPingCustom(device, current_time, ping)
+            current_time += timedelta(seconds=60)
+        
+        device.save()
 
 
 def generate_sofia_data():
@@ -16,11 +40,11 @@ def generate_sofia_data():
     sofia_bucket_data = []
     for i in range(sofia_points):
         sofia_bucket_data.append([])
-        for j in range(80):
+        for j in range(3600):
             sofia_bucket_data[i].append(random() * 20 + 80)
     sofia_data = []
     for i in range(sofia_points):
-        sofia_data.append(DeviceData(id=1,
+        sofia_data.append(DeviceData(id=i,
                                      clusterId=1,
                                      buckets=sofia_bucket_data[i],
                                      location=sofia_local_data[i]
@@ -33,12 +57,12 @@ def generate_varna_data():
     varna_bucket_data = []
     for i in range(varna_points):
         varna_bucket_data.append([])
-        for j in range(80):
+        for j in range(3600):
             varna_bucket_data[i].append(random() * 20 + 80)
     varna_data = []
     for i in range(varna_points):
-        varna_data.append(DeviceData(id=1,
-                                     clusterId=1,
+        varna_data.append(DeviceData(id=i + sofia_points,
+                                     clusterId=2,
                                      buckets=varna_bucket_data[i],
                                      location=varna_local_data[i]
                                      ))
@@ -50,12 +74,12 @@ def generate_burgas_data():
     burgas_bucket_data = [] 
     for i in range(burgas_points):
         burgas_bucket_data.append([])
-        for j in range(80):
+        for j in range(3600):
             burgas_bucket_data[i].append(random() * 20 + 80)
     burgas_data = []
     for i in range(burgas_points):
-        burgas_data.append(DeviceData(id=1,
-                                      clusterId=1,
+        burgas_data.append(DeviceData(id=i + sofia_points + varna_points,
+                                      clusterId=3,
                                       buckets=burgas_bucket_data[i],
                                       location=burgas_local_data[i]
                                       ))
